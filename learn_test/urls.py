73455bf95816295a3b42380ba26862a6089d8bac
“wzzst310” <wzzst310@163.com>
2019-01-13 07:45:09 +0800

    pps

--- learn_test/urls.py
+++ learn_test/urls.py
@@ -4,5 +4,7 @@ from learn_test import views
 # 应用中url配置严格匹配开头和结尾
 urlpatterns = [
     # 通过url函数设置url路由配置项
-    path('index', views.index)
+    path('index', views.index),
+    path('login', views.login),
+    path('login_check', views.login_check)
 ]
--- learn_test/views.py
+++ learn_test/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse, request as r
 
 
 # Create your views here.
 def index(request):
     # i = 1/0;
+    learn()
     return render(request, 'learn_test/index.html')
+
+
+def login(request):
+    return render(request, 'learn_test/login.html')
+
+
+def login_check(request):
+    print(request.POST)
+    username = request.POST['username']
+    password = request.POST['password']
+    print(username + ':' + password)
+    if username == 'wjj' and password == 'wzzst310':
+        return redirect('/learn_test/index')
+    else:
+        return redirect('/learn_test/login')
+    return HttpResponse('OK')
+
+
+def learn():
+    q = r.QueryDict("a=18&a='66'&b='4'")
+    print(q['a'])
+    print(q.get('b'))
