3d4f90160579e37e0f9e5285244ea5b7fabb8fdb
“wzzst310” <wzzst310@163.com>
2019-01-16 07:57:55 +0800

    pps

--- pps/views.py
+++ pps/views.py
@@ -47,7 +47,7 @@ def temp_escape(request):
 
 
 def login(request):
-    if request.session.has_key('islogin'):
+    if request.session['islogin']:
         return redirect('pps/change_pwd')
     else:
         if 'username' in request.COOKIES:
@@ -82,7 +82,7 @@ def login_required(view_func):
     '''登录判断装饰器'''
 
     def wrapper(request, *view_args, **view_kwargs):
-        if request.session.has_key('islogin'):
+        if request.session['islogin']:
             return view_func(request, *view_args, **view_kwargs)
         else:
             return redirect('/pps/login')
@@ -92,8 +92,8 @@ def login_required(view_func):
 
 @login_required
 def change_pwd(request):
-    if not request.session.has_key('islogin'):
-        return redirect('/pps/login')
+    #    if not request.session.has_key('islogin'):
+    #        return redirect('/pps/login')
     return render(request, 'pps/change_pwd.html')
 
 
